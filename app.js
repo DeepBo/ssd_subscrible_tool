0d28e4b6a584fe81107d0cb1a41edb0193ef5642
DeepBoy <43660949+DeepBo@users.noreply.github.com>
2019-07-10 11:04:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let readSync = util.readSync
 let writeSync = util.writeSync
 
 let BUILD_DIR = 'dist' // 构建目录
-let ENTRY_FILE = './node.txt' // SS(R)入口
+let ENTRY_FILE = './node.txt' // SSD入口
 let str = readSync(ENTRY_FILE)
 
 let checker = item => {
@@ -20,8 +20,5 @@ let checker = item => {
 }
 
 // Map all the item include `ssd://` and serialize those items
-let result = str.split('\n\n')
-                .filter(item => checker(item))
-                .join('\r\n')
 
 writeSync(path.resolve(__dirname, BUILD_DIR, 'index.html'), base64(result))
